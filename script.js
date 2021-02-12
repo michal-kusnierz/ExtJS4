c26a96ac8772321dda75b9407bc36d70bb773d90
michal-kusnierz <kusnierz.michal@gmail.com>
2021-02-12 14:35:31 +0100

    simple styles added to component

--- script.js
+++ script.js
@@ -4,14 +4,18 @@ Ext.application({
     Ext.create('Ext.container.Viewport', {
       items: [
         {
-          // xtype: 'component',
-          // html: 'My First ExtJS Application',
-          padding: 20,
           tpl: 'name: {first} {last}',
           data: {
             first: 'Peter',
             last: 'Kellner'
           },
+          padding: 20,
+          width: 200,
+          height: 200,
+          style: {
+            border: '2px solid red;'
+          },
+          resizable: true,
           listeners: {
             resize: {
               fn: function (component, width, height) {
