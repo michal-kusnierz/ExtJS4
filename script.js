53ebaf4a44e28a9deeb3c176aa1fe3aa8423227a
michal-kusnierz <kusnierz.michal@gmail.com>
2021-02-12 14:23:45 +0100

    Base App set with a basic component

--- script.js
+++ script.js
@@ -1,45 +1,14 @@
+let myComponent = Ext.create('Ext.Component', {
+  html: 'My First ExtJS Application'
+});
+
 Ext.application({
   name: 'MyApp',
   launch: function () {
     Ext.create('Ext.container.Viewport', {
       items: [
-        {
-          xtype: 'component',
-          html: '<p>If you click on me, I will disappear.</p>',
-          listeners: {
-            click: {
-              element: 'el',
-              fn: function() {
-                this.hide();
-              }
-            }
-          }
-        },
-        {
-          xtype: 'component',
-          html: '<p>Click me away!</p>',
-          listeners: {
-            click: {
-              element: 'el',
-              fn: function() {
-                this.hide();
-              }
-            }
-          }
-        },
-        {
-          xtype: 'component',
-          html: '<p>Click me too!</p>',
-          listeners: {
-            click: {
-              element: 'el',
-              fn: function() {
-                this.hide();
-              }
-            }
-          }
-        }
+        myComponent
       ]
-    })
+    });
   }
 });
