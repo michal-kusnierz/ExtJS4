70cdc5baa3ca19c0ab25bf1e0571f5f758528534
michal-kusnierz <kusnierz.michal@gmail.com>
2021-02-12 16:17:31 +0100

    create Ext.container.Container with array of Components

--- script.js
+++ script.js
@@ -1,58 +1,24 @@
+var myComp1 = Ext.create('Ext.Component', {
+  html: 'component1'
+});
+var myComp2 = Ext.create('Ext.Component', {
+  html: 'component2'
+});
+var myComp3 = Ext.create('Ext.Component', {
+  html: 'component3'
+});
+
+var myContainer = Ext.create('Ext.container.Container', {
+  items: [myComp1, myComp2, myComp3],
+  layout: 'vbox'
+});
+
 Ext.application({
   name: 'MyApp',
   launch: function () {
     Ext.create('Ext.container.Viewport', {
       items: [
-        {
-          xtype: 'button',
-          text: 'disable',
-          handler: function() {
-            this.up().down('#myComponent').disable();
-          }
-        },
-        {
-          xtype: 'button',
-          text: 'enable',
-          handler: function() {
-            this.up().down('#myComponent').enable();
-          }
-        },
-        {
-          xtype: 'button',
-          text: 'hide',
-          handler: function() {
-            this.up().down('#myComponent').hide();
-          }
-        },
-        {
-          xtype: 'button',
-          text: 'show',
-          handler: function() {
-            this.up().down('#myComponent').show();
-          }
-        },
-        {
-          itemId: 'myComponent',
-          tpl: 'name: {first} {last}',
-          data: {
-            first: 'Peter',
-            last: 'Kellner'
-          },
-          padding: 20,
-          width: 200,
-          height: 200,
-          style: {
-            border: '2px solid red;'
-          },
-          resizable: true,
-          listeners: {
-            resize: {
-              fn: function (component, width, height) {
-                console.log('w/h: ' + width + ' ' + height);
-              }
-            }
-          }
-        }
+        myContainer
       ]
     });
   }
