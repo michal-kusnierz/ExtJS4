0ccb82b768c3552dff4e346e111092884ce2b762
michal-kusnierz <kusnierz.michal@gmail.com>
2021-02-12 14:45:33 +0100

    added button

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ Ext.application({
     Ext.create('Ext.container.Viewport', {
       items: [
         {
+          xtype: 'button',
+          text: 'disable'
+        },
+        {
           tpl: 'name: {first} {last}',
           data: {
             first: 'Peter',
