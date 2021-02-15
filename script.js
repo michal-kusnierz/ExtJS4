1b60afbd5c6c7b4b78fa029336ac856bc143db94
michal-kusnierz <kusnierz.michal@gmail.com>
2021-02-15 14:38:03 +0100

    app: added 3 Panels to Viewport (items array)

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+let sessionsPanel = Ext.create('Ext.Panel', {
+  html: 'Sessions Panel'
+}),
+detailsPanel = Ext.create('Ext.Panel', {
+  html: 'Details Panel'
+}),
+speakersPanel = Ext.create('Ext.Panel', {
+  html: 'Speakers Panel'
+});
+
 Ext.application({
   name: 'App',
   launch: function() {
     Ext.create('Ext.container.Viewport', {
-      html: "Howdy!"
+      html: "Howdy!",
+      items: [sessionsPanel,detailsPanel,speakersPanel]
     });
   }
 });
