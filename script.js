057782991ac03727b6835780116928424a5010a6
michal-kusnierz <kusnierz.michal@gmail.com>
2021-02-15 14:50:29 +0100

    app: combined layouts

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 let sessionsPanel = Ext.create('Ext.Panel', {
   html: 'Sessions Panel',
-  region: 'west',
   flex: 2
 }),
 detailsPanel = Ext.create('Ext.Panel', {
   html: 'Details Panel',
-  region: 'center',
   flex: 3
 }),
 speakersPanel = Ext.create('Ext.Panel', {
   html: 'Speakers Panel',
-  region: 'south',
   flex: 1
+}),
+combinedSessionSpeaker = Ext.create('Ext.Panel', {
+  layout: {
+    type: 'vbox'
+  },
+  items: [sessionsPanel,speakersPanel]
 });
 
 Ext.application({
@@ -19,8 +22,11 @@ Ext.application({
   launch: function() {
     Ext.create('Ext.container.Viewport', {
       html: "Howdy!",
-      layout: 'border',
-      items: [sessionsPanel,detailsPanel,speakersPanel]
+      layout: {
+        type: 'hbox',
+        align: 'stretch'
+      },
+      items: [combinedSessionSpeaker,detailsPanel]
     });
   }
 });
