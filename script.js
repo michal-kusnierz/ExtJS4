8ee6c092d8693176013eb9490f49a0ff785905d9
michal-kusnierz <kusnierz.michal@gmail.com>
2021-02-15 12:00:05 +0100

    Ext.data.Model

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
-var myComp1 = Ext.create('Ext.Component', {
-  html: 'component1'
-});
-var myComp2 = Ext.create('Ext.Component', {
-  html: 'component2'
-});
-var myComp3 = Ext.create('Ext.Component', {
-  html: 'component3'
-});
-
-var myContainer = Ext.create('Ext.container.Container', {
-  items: [myComp1, myComp2, myComp3],
-  layout: 'vbox'
-});
-
-Ext.application({
-  name: 'MyApp',
-  launch: function () {
-    Ext.create('Ext.container.Viewport', {
-      items: [
-        myContainer
-      ]
-    });
-  }
+Ext.define('SessionModel', {
+  extend: 'Ext.data.Model',
+  fields: ['id','title','sessionLevel','approved']
 });
