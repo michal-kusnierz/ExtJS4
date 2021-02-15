83dcf0c7e538587a7e9f93536ec7da1bf84bb32e
michal-kusnierz <kusnierz.michal@gmail.com>
2021-02-15 12:12:43 +0100

    added validation to data.Model

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ Ext.define('SessionModel', {
     { name: 'title', type: 'string' },
     { name: 'sessionLevel', type: 'int' },
     { name: 'approved', type: 'bool', defaultValue: false }
+  ],
+  validations: [
+    { type: 'length', field: 'title', min: 3 },
+    { type: 'inclusion', field: 'sessionLevel', list: [1,2,3] }
   ]
 });
 
@@ -12,6 +16,14 @@ let mySession1 = Ext.create('SessionModel', {
   title: 'C++',
   sessionLevel: 2
 });
+
+if (!mySession1.isValid()) {
+  let errors = mySession1.validate();
+  errors.for(function(rec) {
+    console.log(rec);
+  });
+}
+
 let mySession2 = Ext.create('SessionModel', {
   title: 'C#',
   sessionLevel: 3
