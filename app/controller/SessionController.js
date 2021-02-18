f661b4325e2c06c3e200c9a8c70075f93032a62f
michal-kusnierz <kusnierz.michal@gmail.com>
2021-02-18 16:03:21 +0100

    mvc: added select fn into controller for sessiongridpanel

--- app/controller/SessionController.js
+++ app/controller/SessionController.js
@@ -13,6 +13,26 @@ Ext.define('SE.controller.SessionController', {
           let form = formWindow.down('form');
           form.loadRecord(record);
           formWindow.show();
+        },
+        select: function(rowmodel, record, index, eOpts) {
+          let sessionId = record.get('id');
+          let presenterIds = [];
+          let spStore = this.getSessionPresentersStore();
+          spStore.each(function (rec) {
+            if (rec.get('sessionId') === sessionId) {
+              presenterIds.push(rec.get('presenterId'));
+            }
+          });
+          this.getPresentersStore().clearFilter();
+          this.getPresentersStore().filterBy(function(rec) {
+            let foundMatch = false;
+            for (let i = 0; i < presenterIds.length; i++) {
+              if (rec.get('id') === presenterIds[i]) {
+                foundMatch = true;
+              }
+            }
+            return foundMatch;
+          })
         }
       }
     });
