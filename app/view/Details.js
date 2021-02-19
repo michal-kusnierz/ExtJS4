cceb6478c148fef47952bd9f65a673f4c07d9281
michal-kusnierz <kusnierz.michal@gmail.com>
2021-02-19 15:39:01 +0100

    mvc: using tutor's template to render data prettier

--- app/view/Details.js
+++ app/view/Details.js
@@ -6,13 +6,39 @@ Ext.define('SE.view.Details', {
   bodyPadding: 10,
 
   tpl: [
-    '<br>Title: {title}',
-    '<br>Description: {description}',
-    '<br>',
-    '<tpl for="presenters">',   // this tpl functionality iterates over an attribute 
-    ' <br>Presenter: {firstName} {lastName}',
-    ' <br><img class="leaderPhoto" src="http://www.siliconvalley-codecamp.com/{imageUrl}?width=150">',
-    '</tpl>'
+    '<section class="pageContent">',
+    '',
+    '    <h2 class="purpleRibbon">Session Detail</h2>',
+    '',
+    '    <section class="workshopInfo">',
+    '        <h2>{title}</h2>',
+    '        <section class="presentationBox">',
+    '            <p>{description}</p>',
+    '        </section>',
+    '    </section>',
+    '    <section class="workshopLeader">',
+    '        <h2>Speaker(s)</h2>',
+    '        <div class="inner">',
+    '            <tpl for="presenters">',
+    '                    <tpl>',
+    '                        <div class="leader">',
+    '                            <div class="leftColumn">',
+    '                                <img class="leaderPhoto" src="http://www.siliconvalley-codecamp.com/{imageUrl}?format=jpg&width=175&Height=175&scale=both&mode=crop" alt="{firstName} {lastName}" />',
+    '                                <p class="leaderName">{firstName} {lastName}</p>',
+    '                                <p class="leaderURL"><a href="{webSite}">{webSite}</a></p>',
+    '                            </div>',
+    '',
+    '                            <div class="rightColumn">',
+    '                                <h3>{firstName} {lastName}</h3>',
+    '                                <div>{bio}</div>',
+    '                            </div>',
+    '                        </div>',
+    '                    </tpl>',
+    '                </tpl>',
+    '',
+    '        </div>',
+    '    </section>',
+    '</section>'
   ]
 
 });
